e404b2ac46176422dc0515b6ddb1b1a4e65ca35c
aman-coder28 <amancoder28@gmail.com>
2026-07-30 14:34:50 -0400

    Parse and Stringify Data
    Signed-off-by: aman-coder28 <amancoder28@gmail.com>

--- src/files.go
+++ src/files.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"errors"
+	"encoding/json"
 	"os"
 )
 
-func LoadFile(fileName string) (string, error) {
+func LoadFile(fileName string) ([]byte, error) {
 	if file, err := os.ReadFile(fileName); err != nil {
-		return "", errors.New(err.Error())
+		return []byte(""), err
 	} else {
-		return string(file), nil
+		return file, nil
+	}
+}
+
+type Input struct {
+	Data string `json:"data"`
+}
+
+func ParseJson(data []byte) (*Input, error) {
+	var input Input
+
+	if err := json.Unmarshal(data, &input); err != nil {
+		return nil, err
+	} else {
+		return &input, nil
+	}
+}
+
+func StingifyData(input Input) ([]byte, error) {
+	if data, err := json.Marshal(input); err != nil {
+		return []byte(""), err
+	} else {
+		return data, nil
 	}
 }
--- src/main.go
+++ src/main.go
@@ -1,14 +1,27 @@
 package main
 
-func main() {
-	// scanner := bufio.NewScanner(os.Stdin)
+import "fmt"
 
-	// RunCalculator(scanner)
-	//
-	if file, err := LoadFile("../.env"); err != nil {
-		println(err.Error())
-	} else {
-		println(file)
+func main() {
+	file, fileErr := LoadFile("../.env")
+
+	if fileErr != nil {
+		fmt.Println(fileErr.Error())
 	}
 
+	input, inputErr := ParseJson(file)
+
+	if inputErr != nil {
+		fmt.Println(inputErr.Error())
+	}
+
+	fmt.Println(input.Data)
+
+	data, err := StingifyData(*input)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(string(data))
 }
